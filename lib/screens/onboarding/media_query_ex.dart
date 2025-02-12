cd92f9e59862c4a4168a063e4817df86df1a7f98
Viren Gajjar <vgajjar1010@gmail.com>
2025-02-12 11:24:32 +0530

    feat: learn container

--- lib/screens/onboarding/media_query_ex.dart
+++ lib/screens/onboarding/media_query_ex.dart
@@ -13,11 +13,41 @@ class _MediaQueryExState extends State<MediaQueryEx> {
     return Scaffold(
       body: Center(
         child: Container(
-          height:  MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          color: Colors.yellow,
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            border: Border.all(
+              color: Colors.red,
+              width: 10,
+              strokeAlign: BorderSide.strokeAlignInside,
+            ),
+            image: const DecorationImage(
+              image: ExactAssetImage(
+                'assets/images/onboarding.png',
+              ),
+            ),
+            gradient: const LinearGradient(colors: [
+              Colors.red,
+              Colors.yellow,
+              Colors.black,
+            ]),
+            shape: BoxShape.rectangle,
+            boxShadow: const [
+              BoxShadow(
+                blurRadius: 100,
+                blurStyle: BlurStyle.outer,
+                spreadRadius: 1,
+                offset: Offset(10, 100),
+                color: Colors.black,
+              ),
+            ],
+          ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          padding: const EdgeInsets.all(20),
           child: const Text(
-            "We are learning MediaQuery",
+            "Hello hi there jhfdjhfdjhfdjhfjhdfjdhf\n therjehrjh \n fdjhdjfhdjfdh jhfdjhfdf",
             style: TextStyle(
               fontSize: 34,
               fontWeight: FontWeight.bold,
